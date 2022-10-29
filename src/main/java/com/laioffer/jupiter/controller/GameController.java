5b5e58cb76e19ec5a8c5c2e57f1a06b06e6844c1
Zhenlin Wang <djj87018507@gmail.com>
2022-10-29 19:10:35 -0400

    Configure Game Search and Item Search features

--- src/main/java/com/laioffer/jupiter/controller/GameController.java
+++ src/main/java/com/laioffer/jupiter/controller/GameController.java
@@ -1,32 +1,36 @@
 package com.laioffer.jupiter.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.jupiter.service.GameService;
+import com.laioffer.jupiter.service.TwitchException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Controller
 public class GameController {
-    @RequestMapping(value="/game", method= RequestMethod.GET)
-    public void getGame(@RequestParam(value = "game_name", required = false) String
-                        gameName, HttpServletResponse response) throws IOException {
-        System.out.println(gameName);
+
+    @Autowired
+    private GameService gameService;
+
+    @RequestMapping(value = "/game", method = RequestMethod.GET)
+    public void getGame(@RequestParam(value = "game_name", required = false) String gameName,
+                        HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("application/json;charset=UTF-8");
+        try {
+            // Return the dedicated game information if gameName is provided in the request URL, otherwise return the top x games.
+            if (gameName != null) {
+                response.getWriter().print(new ObjectMapper().writeValueAsString(gameService.searchGame(gameName)));
+            } else {
+                response.getWriter().print(new ObjectMapper().writeValueAsString(gameService.topGames(0)));
+            }
+        } catch (TwitchException e) {
+            throw new ServletException(e);
+        }
+
     }
-
-    @RequestMapping(value="/search", method= RequestMethod.GET)
-    public String getSearch(@RequestParam(value = "lon", required = false) String lon
-            , HttpServletResponse response) throws IOException {
-        System.out.println(lon);
-        return "game";
-    }
-
-    @RequestMapping(value = "/restaurant/{id}/menu", method = RequestMethod.GET)
-    public void searchMenu(@PathVariable("id") int id) {
-
-    }
-
 }
--- src/main/java/com/laioffer/jupiter/controller/SearchController.java
+++ src/main/java/com/laioffer/jupiter/controller/SearchController.java
@@ -1,20 +1,25 @@
 package com.laioffer.jupiter.controller;
 
+import com.laioffer.jupiter.entity.db.Item;
+import com.laioffer.jupiter.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class SearchController {
-//    @RequestMapping(value="/search", method= RequestMethod.GET)
-//    public void getSearch(@RequestParam(value = "game_name", required = false) String
-//                                        gameName, HttpServletResponse response) throws IOException {
-////        System.out.println(lon);
-////        return "hola";
-//        System.out.println(gameName);
-//    }
+
+    @Autowired
+    private GameService gameService;
+
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, List<Item>> search(@RequestParam(value = "game_id") String gameId) {
+        return gameService.searchItems(gameId);
+    }
 }
